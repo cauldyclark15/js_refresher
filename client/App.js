8ae4a11b4dc60c8ab2cba8dfa46c12518ab47b48
cauldyclark15 <jcing.jc@gmail.com>
2017-06-24 10:00:23 +0800

    Added routing

--- client/App.js
+++ client/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import configureStore from './store'
 import Posts from './posts/Posts'
+import Users from './users/Users'
+import Landing from './landing'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/style/main.css'
@@ -12,7 +15,20 @@ var store = configureStore()
 
 const App = () => (
   <Provider store={store}>
-    <Posts />
+    <BrowserRouter>
+      <div>
+        <ul className='nav nav-tabs'>
+          <li role='presentation'><Link to='/'>Home</Link></li>
+          <li role='presentation'><Link to='/users'>Users</Link></li>
+          <li role='presentation'><Link to='/posts'>Posts</Link></li>
+        </ul>
+        <Switch>
+          <Route exact path='/' component={Landing} />
+          <Route path='/posts' component={Posts} />
+          <Route path='/users' component={Users} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
 )
 
---
+++ client/landing/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Landing = () => (
+  <div>
+    <h1>React Refresher</h1>
+    <p>This is a simple refresher before the biggest grind of my career.</p>
+  </div>
+)
+
+export default Landing
